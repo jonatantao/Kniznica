0de981f4352d8b080cc40b4731454de439b2d2f2
jonatantao <michelik.jony@gmail.com>
2022-02-22 17:15:58 +0100

    controllerBook init

--- src/main/java/com/example/demo/controllerBook.java
+++ src/main/java/com/example/demo/controllerBook.java
@@ -6,13 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class controller {
+public class controllerBook {
     List<Book> books;
-    List<User> users;
 
-    public controller(){
+    public controllerBook(){
         this.books = init();
-        this.users = init2();
     }
 
     public List<Book> init() {
@@ -33,59 +31,6 @@ public class controller {
         books.add(book2);
 
         return books;
-    }
-
-    public List<User> init2(){ //init2 list of users
-        List<User> users = new ArrayList<>();
-
-        User user1 = new User();
-        user1.setId("1");
-        user1.setFirstName("Jožo");
-        user1.setLastName("Alvaréz");
-        user1.setEmail("j.alvarez@gmail.com");
-        users.add(user1);
-
-        User user2 = new User();
-        user2.setId("2");
-        user2.setFirstName("Jano");
-        user2.setLastName("Odvedľa");
-        user2.setEmail("odvedla@gmail.com");
-        users.add(user2);
-        return users;
-    }
-
-    @GetMapping("/api/users") //filtered users with user last name
-    public List<User> getUsers(@RequestParam(required = false) String userlastName){
-        if (userlastName == null){
-            return this.users;
-        }
-
-        List<User> filteredUsers = new ArrayList<>();
-
-        for (User user : users){
-            if (user.getLastName().equals(userlastName)){
-                filteredUsers.add(user);
-            }
-        }
-
-        return filteredUsers;
-    }
-
-    @GetMapping("/api/userid") //filtered users with user last name
-    public List<User> getUsersId(@RequestParam(required = false) String userId){
-        if (userId == null){
-            return this.users;
-        }
-
-        List<User> filteredUsers = new ArrayList<>();
-
-        for (User user : users){
-            if (user.getId().equals(userId)){
-                filteredUsers.add(user);
-            }
-        }
-
-        return filteredUsers;
     }
 
     @GetMapping("/api/books")
@@ -150,23 +95,10 @@ public class controller {
         return books;
     }
 
-    @PostMapping("/api/users") //creating new user
-    public List<User> createUser(@RequestBody User user){
-        this.users.add(user);
-
-        return users;
-    }
-
     @DeleteMapping("/api/books/{bookId}")
     public void deleteBook(@PathVariable Integer bookId){
         this.books.remove(this.books.get(bookId));
     }
-
-    @DeleteMapping("/api/users/{userId}")
-    public void deleteUser(@PathVariable Integer userId){
-        this.users.remove(this.users.get(userId));
-    }
-
 
     @PutMapping("/api/books/{bookId}") //update by {book}
     public List<Book> putBook(@PathVariable Integer bookId, @RequestBody Book book){
@@ -175,14 +107,5 @@ public class controller {
         this.books.get(bookId).setTitle(book.getTitle());
         this.books.get(bookId).setIsbn(book.getIsbn());
         return books;
-    }
-
-    @PutMapping("/api/users/{userId}")
-    public List<User> putUser(@PathVariable Integer userId, @RequestBody User user){
-        this.users.get(userId).setId(user.getId());
-        this.users.get(userId).setFirstName(user.getFirstName());
-        this.users.get(userId).setLastName(user.getLastName());
-        this.users.get(userId).setEmail(user.getEmail());
-        return users;
     }
 }
