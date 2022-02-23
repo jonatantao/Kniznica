e93338ab9fe36108c9a1aa5a45d06bf6a246f4d6
jonatantao <michelik.jony@gmail.com>
2022-02-23 15:51:03 +0100

    BookService init

--- src/main/java/com/example/demo/controllerBook.java
+++ src/main/java/com/example/demo/controllerBook.java
@@ -7,105 +7,46 @@ import java.util.List;
 
 @RestController
 public class controllerBook {
-    List<Book> books;
+    private List<Book> books;
 
-    public controllerBook(){
-        this.books = init();
-    }
+    private BookService bookService;
 
-    public List<Book> init() {
-        List<Book> books = new ArrayList<>();
-
-        Book book1 = new Book();
-        book1.setAuthor("Haruki Murakami");
-        book1.setTitle("Norwegian Wood");
-        book1.setIsbn("0099448823");
-        book1.setId("1");
-        books.add(book1);
-
-        Book book2 = new Book();
-        book2.setAuthor("Conan");
-        book2.setTitle("Cau");
-        book2.setIsbn("456");
-        book2.setId("2");
-        books.add(book2);
-
-        return books;
+    public controllerBook(BookService bookService){
+        this.bookService = bookService;
     }
 
     @GetMapping("/api/books")
     public List<Book> getBooks(@RequestParam(required = false) String bookAuthor){
-        if (bookAuthor == null){
-            return this.books;
-        }
-
-        List<Book> filteredBooks = new ArrayList<>();
-
-        for (Book book : books){
-            if (book.getAuthor().equals(bookAuthor)){
-                filteredBooks.add(book);
-            }
-        }
-
-        return filteredBooks;
+        return bookService.getBooks(bookAuthor);
     }
 
     @GetMapping("/api/book/{bookId}") //search by {bookId}
     public Book getBook(@PathVariable Integer bookId){
-        return this.books.get(bookId);
+        return this.bookService.getBook(bookId);
     }
 
     @GetMapping("/api/id") //search by bookId
     public List<Book> getBookId(@RequestParam(required = false) String bookId){
-        if (bookId == null){
-            return this.books;
-        }
-
-        List<Book> filteredBooks = new ArrayList<>();
-
-        for (Book book : books){
-            if (book.getId().equals(bookId)){
-                filteredBooks.add(book);
-            }
-        }
-
-        return filteredBooks;
+        return bookService.getBookId(bookId);
     }
 
     @GetMapping("/api/isbn") //search by bookIsbn
     public List<Book> getBookIsbn(@RequestParam(required = false) String bookIsbn){
-        if (bookIsbn == null){
-            return this.books;
-        }
-
-        List<Book> filteredBooks = new ArrayList<>();
-
-        for (Book book : books){
-            if (book.getIsbn().equals(bookIsbn)){
-                filteredBooks.add(book);
-            }
-        }
-
-        return filteredBooks;
+        return bookService.getBookIsbn(bookIsbn);
     }
 
     @PostMapping("/api/books") //create new book
     public List<Book> createBook(@RequestBody Book book){
-        this.books.add(book);
-        return books;
+        return bookService.createBook(book);
     }
 
     @DeleteMapping("/api/books/{bookId}")
     public void deleteBook(@PathVariable Integer bookId){
-        this.books.remove(this.books.get(bookId));
+        this.bookService.deleteBook(bookId);
     }
 
     @PutMapping("/api/books/{bookId}") //update by {book}
     public List<Book> putBook(@PathVariable Integer bookId, @RequestBody Book book){
-        this.books.get(bookId).setId(book.getId());
-        this.books.get(bookId).setAuthor(book.getAuthor());
-        this.books.get(bookId).setTitle(book.getTitle());
-        this.books.get(bookId).setIsbn(book.getIsbn());
-        return books;
+        return this.bookService.putBook(bookId, book);
     }
 }
